4dc54a9202262edce8386c7c9970ee73af4f1eef
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-02-10 22:00:15 -0500

    Included relactionship on tables

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -10,12 +10,22 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class PostType extends AbstractType
 {
   public function buildForm(FormBuilderInterface $builder, array $options): void
   {
     $builder
+      ->add(
+        'category',
+        ChoiceType::class,
+        [
+          'choices' => ['PHP' => 'php', 'Laravel' => 'laravel', 'Symfony' => 'symfony'],
+          'placeholder' => 'Select a Category',
+          'label' => 'Categories'
+        ]
+      )
       ->add('title', TextType::class, ['label' => 'Post title', 'help' => 'Think about SEO, how do you search on Google?'])
       ->add('body', TextareaType::class, ['label' => 'Content', 'attr' => ['rows' => 9, 'class' => 'bg-light']])
       ->add('Send', SubmitType::class, ['attr' => ['class' => 'btn btn-dark']]);
