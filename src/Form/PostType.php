ae9641cc3fad9be5fd4722b1ba98315272084351
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-02-10 21:10:56 -0500

    Form connect with entity

---
+++ src/Form/PostType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class PostType extends AbstractType
+{
+  public function buildForm(FormBuilderInterface $builder, array $options): void
+  {
+    $builder
+      ->add('title', TextType::class, ['label' => 'Post title', 'help' => 'Think about SEO, how do you search on Google?'])
+      ->add('body', TextareaType::class, ['label' => 'Content', 'attr' => ['rows' => 9, 'class' => 'bg-light']])
+      ->add('Send', SubmitType::class, ['attr' => ['class' => 'btn btn-dark']]);
+  }
+
+  public function configureOptions(OptionsResolver $resolver): void
+  {
+    $resolver->setDefaults([
+      'data_class' => Post::class,
+      //'csrf_field_name' => '_custom_token'
+      //'csrf_protection' => false,
+    ]);
+  }
+}
