8accc62113e1a2f2d1389cad0be9f379ff44cd8d
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-02-10 23:00:51 -0500

    Ediing info

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Form\PostType;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -12,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
-  #[Route('/post/create', name: 'post_create')]
+  #[Route('/post/create', name: 'post_create', methods: ['GET', 'POST'])]
   public function create(Request $request, EntityManagerInterface $entityManager): Response
   {
     $form = $this->createForm(PostType::class);
@@ -28,6 +29,26 @@ class PostController extends AbstractController
     }
 
     return $this->render('post/create.html.twig', [
+      'form' => $form->createView(),
+    ]);
+  }
+
+  #[Route('/post/{id}/edit', name: 'post_edit', methods: ['GET', 'POST'])]
+  public function edit(Post $post, Request $request, EntityManagerInterface $entityManager): Response
+  {
+    $form = $this->createForm(PostType::class, $post);
+
+    $form->handleRequest($request);
+    if ($form->isSubmitted()) {
+      $name = $form->getData();
+      //$entityManager->persist($form->getData()); Optional line
+      $entityManager->flush();
+
+      $this->addFlash('success', "the Pots '{$name->getTitle()}' has been edited successfull");
+      return $this->redirectToRoute('post_edit', ['id' => $post->getId()]);
+    }
+
+    return $this->render('post/edit.html.twig', [
       'form' => $form->createView(),
     ]);
   }
